2d8a9aa54a04c06119f3d917fcfa1741bf2867f7
Max Gasner <max@priorknowledge.com>
2012-05-22 08:14:27 -0700

    add options

--- lib/veritable.rb
+++ lib/veritable.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 require 'openssl'
 require 'json'
 
+require 'veritable/api'
 require 'veritable/connection'
 require 'veritable/version'
 
@@ -15,10 +16,13 @@ module Veritable
     api_key = opts[:api_key] || ENV['VERITABLE_KEY']
     api_url = opts[:api_url] || ENV['VERITABLE_URL'] || "https://api.priorknowledge.com"
 
+    opts.has_key? :ssl_verify ? ssl_verify = opts[:ssl_verify] : ssl_verify = true
+    opts.has_key? :enable_gzip ? enable_gzip = opts[:enable_gzip] : enable_gzip = true
 
     api = API.new(Connection.new(api_key, api_url, opts={:ssl_verify => ssl_verify,
       :enable_gzip => enable_gzip}))
     r = api.root
+    api
   end
   
   class VeritableResource
