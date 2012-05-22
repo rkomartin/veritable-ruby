187e21e82f383bd56bc8299b451baebde0b77082
Max Gasner <max@priorknowledge.com>
2012-05-22 09:01:07 -0700

    delint

--- test/test_veritable.rb
+++ test/test_veritable.rb
@@ -2,39 +2,42 @@ require 'test/unit'
 require 'veritable'
 
 class VeritableTest < Test::Unit::TestCase
-	def test_connect
-		Veritable.connect()
-	end
+  def test_connect
+  end
 
-	def test_connect_explicit
-	end
+  def test_connect_unauthorized_fails
+    assert_raise(VeritableError, Veritable.connect)
+  end
 
-	def test_connect_nogzip
-	end
+  def test_connect_explicit
+  end
 
-	def test_connect_ssl_verify_fails
-	end
+  def test_connect_nogzip
+  end
 
-	def test_connect_not_api_fails
-	end
+  def test_connect_ssl_verify_fails
+  end
 
-	def test_instantiate_veritable_resource
-	end
+  def test_connect_not_api_fails
+  end
 
-	def test_instantiate_api
-	end
-	
-	def test_api_root
-	end
+  def test_instantiate_veritable_resource
+  end
 
-	def test_api_limits
-	end
+  def test_instantiate_api
+  end
+  
+  def test_api_root
+  end
 
-	def test_api_list_tables
-	end
+  def test_api_limits
+  end
+
+  def test_api_list_tables
+  end
 end
 
 class VeritableTestConnection < Test::Unit::TestCase
-	def test_instantiate
-	end
+  def test_instantiate
+  end
 end
