8b8293360e28a05480bf60fe9914967b017a8358
Max Gasner <max@priorknowledge.com>
2012-05-22 14:25:51 -0700

    link method

--- lib/veritable/api.rb
+++ lib/veritable/api.rb
@@ -1,7 +1,15 @@
 require 'veritable/connection'
+require 'veritable/errors'
 
 module Veritable
   class VeritableResource < Connection
+
+    private
+
+    def link(name)
+      raise VeritableError unless @doc['links'].has_key?(name)
+      @doc['links'][name]
+    end
   end
 
   class API < VeritableResource
