9fa413eea268561e971dbf2a54576d9dba7f6869
Max Gasner <max@priorknowledge.com>
2012-05-22 12:16:26 -0700

    use a single options hash

--- lib/veritable.rb
+++ lib/veritable.rb
@@ -14,9 +14,9 @@ require 'rest_client'
 module Veritable
   USER_AGENT = 'veritable-ruby ' + VERSION
   
-  def self.connect(api_key=nil, api_url=nil, opts={})
-    opts[:api_key] = api_key || opts[:api_key] || ENV['VERITABLE_KEY']
-    opts[:api_url] = api_url || opts[:api_url] || ENV['VERITABLE_URL'] || "https://api.priorknowledge.com"
+  def self.connect(opts={})
+    opts[:api_key] = opts[:api_key] || ENV['VERITABLE_KEY']
+    opts[:api_url] = opts[:api_url] || ENV['VERITABLE_URL'] || "https://api.priorknowledge.com"
 
     opts[:ssl_verify] = true unless opts.has_key?(:ssl_verify)
     opts[:enable_gzip] = true unless opts.has_key?(:enable_gzip)
