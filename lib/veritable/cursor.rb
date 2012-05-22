18cd7b3b8f424cfe2afeb02ad3ebeda96fe3c756
Max Gasner <max@priorknowledge.com>
2012-05-22 14:48:26 -0700

    complete cursor draft

--- lib/veritable/cursor.rb
+++ lib/veritable/cursor.rb
@@ -11,8 +11,22 @@ module Veritable
       default_opts({'per_page' => 100})
 
       collection_key = collection.split("/")[-1]
-      @doc = get(collection, params={:per_page => per_page, :start => start})
+      @doc = get(collection, params={:count => per_page, :start => start})
       @doc.has_key?(collection_key) ? @opts['key'] = collection_key : @opts['key'] = 'data'
+    end
+
+    def next
+      if data.length > 0 or refresh > 0
+        if limit
+          if limit == 0
+            raise StopIteration
+          end
+          limit = limit - 1
+        end
+        return data.shift
+      else
+        raise StopIteration
+      end
     end
 
     def inspect; to_s; end
@@ -20,7 +34,20 @@ module Veritable
 
     private
 
+    def refresh
+      return data.length if data.length > 0
+      if next_page
+        @doc = get next_page
+      elsif last_page?
+        return 0
+      else
+        @doc = get(collection, params={:count => per_page, :start => start})
+      end
+      return data.length
+    end
+
     def limit; @opts['limit']; end
+    def limit=(x); @opts['limit'] = x; end
     def start; @opts['start']; end
     def per_page; @opts['per_page']; end
     def collection; @opts['collection'] end
