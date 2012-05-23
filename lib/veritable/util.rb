19f590424c31b6ff307478999032b6433127c9eb
Max Gasner <max@priorknowledge.com>
2012-05-23 10:37:47 -0700

    factor

--- lib/veritable/util.rb
+++ lib/veritable/util.rb
@@ -6,33 +6,40 @@ module Veritable
     class << self
       def make_table_id; UUID.new.generate :compact ; end
       def make_analysis_id; UUID.new.generate :compact ; end
-      def urlencode(k)
-        URI.escape(k.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
-      end
 
       def query_params(params, parent=nil)
+        flatten_params(params).collect {|x|
+          "#{x[0]}=#{x[1]}"
+        }.join("&")
+      end
+
+      private
+
+      def flatten_params(params, parent=nil)
         result = []
         if params.is_a? Hash
           params.each {|k, v|
             kk = parent ? "#{parent}[#{urlencode(k)}]" : urlencode(k)
-            if v.is_a? Hash or v.is_a? Array
-              result += query_params(v, kk)
+            if v.is_a?(Hash) or v.is_a?(Array)
+              result += flatten_params(v, kk)
             else
               result << [kk, urlencode(v)]
             end
           }
         elsif params.is_a? Array
           params.each {|v|
-            if v.is_a? Hash or v.is_a? Array
-              result += query_params(v, kk)
+            if v.is_a?(Hash) or v.is_a?(Array)
+              result += flatten_params(v, kk)
             else
               result << ["#{parent}[]", urlencode(v)]
             end
           }
         end
-        result.collect {|x|
-          "#{x[0]}=#{x[1]}"
-        }.join("&")
+        result
+      end
+
+      def urlencode(k)
+        URI.escape(k.to_s, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]"))
       end
     end
   end
