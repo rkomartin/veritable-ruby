72cac6cd9f9c34d2b8ec6a7f922f447c79c322a5
Max Gasner <max@priorknowledge.com>
2012-05-25 13:24:09 -0400

    fix tests

--- test/test_veritable_tableop.rb
+++ test/test_veritable_tableop.rb
@@ -20,12 +20,12 @@ class VeritableTableOpTest < Test::Unit::TestCase
       ])
     @t2 = @api.create_table
     @t2.batch_upload_rows(
-      [{'_id' => 'row1', 'cat' => 'a', 'ct' => 0, 'real' => 1.02394, 'bool' => True},
-       {'_id' => 'row2', 'cat' => 'b', 'ct' => 0, 'real' => 0.92131, 'bool' => False},
-       {'_id' => 'row3', 'cat' => 'c', 'ct' => 1, 'real' => 1.82812, 'bool' => True},
-       {'_id' => 'row4', 'cat' => 'c', 'ct' => 1, 'real' => 0.81271, 'bool' => True},
-       {'_id' => 'row5', 'cat' => 'd', 'ct' => 2, 'real' => 1.14561, 'bool' => False},
-       {'_id' => 'row6', 'cat' => 'a', 'ct' => 5, 'real' => 1.03412, 'bool' => False}
+      [{'_id' => 'row1', 'cat' => 'a', 'ct' => 0, 'real' => 1.02394, 'bool' => true},
+       {'_id' => 'row2', 'cat' => 'b', 'ct' => 0, 'real' => 0.92131, 'bool' => false},
+       {'_id' => 'row3', 'cat' => 'c', 'ct' => 1, 'real' => 1.82812, 'bool' => true},
+       {'_id' => 'row4', 'cat' => 'c', 'ct' => 1, 'real' => 0.81271, 'bool' => true},
+       {'_id' => 'row5', 'cat' => 'd', 'ct' => 2, 'real' => 1.14561, 'bool' => false},
+       {'_id' => 'row6', 'cat' => 'a', 'ct' => 5, 'real' => 1.03412, 'bool' => false}
       ])
   end
 
@@ -35,14 +35,14 @@ class VeritableTableOpTest < Test::Unit::TestCase
   end
 
   def test_get_row
-    assert @t.get_row("sixbug") == {'_id' => 'sixbug', 'zim' => 'zop', 'wos' => 18.9}
-    assert @t.get_row("fivebug") == {'_id' => 'fivebug', 'zim' => 'zam', 'wos' => 9.3}
+    assert @t.row("sixbug") == {'_id' => 'sixbug', 'zim' => 'zop', 'wos' => 18.9}
+    assert @t.row("fivebug") == {'_id' => 'fivebug', 'zim' => 'zam', 'wos' => 9.3}
   end
 
   def test_add_duplicate_rows
     @t.upload_row({'_id' => 'threebug', 'zim' => 'fop', 'wos' => 17.5})
     @t.upload_row({'_id' => 'threebug', 'zim' => 'vim', 'wos' => 11.3})
-    assert @t.get_row("threebug") == {'_id' => 'threebug', 'zim' => 'vim', 'wos' => 11.3}
+    assert @t.row("threebug") == {'_id' => 'threebug', 'zim' => 'vim', 'wos' => 11.3}
   end
 
   def test_batch_get_rows
@@ -54,12 +54,20 @@ class VeritableTableOpTest < Test::Unit::TestCase
   end
 
   def test_batch_get_rows_limits
-    assert @t.rows({'start' => 'onebug', 'limit' => 0}).count == 0
-    assert @t.rows({'start' => 'onebug', 'limit' => 3}).count == 3
-    assert @t.rows({'start' => 'onebug', 'limit' => 100}).count == 4
-    assert @t2.rows({'start' => 'row0'}).count == 6
-    assert @t2.rows({'start' => 'row7'}).count == 0
+    {{'start' => 'onebug', 'limit' => 0} => 0,
+     {'start' => 'onebug', 'limit' => 3} => 3,
+     {'start' => 'onebug', 'limit' => 100} => 4,
+     {'start' => 'row0'} => 6,
+     {'start' => 'row7'} => 0
+   }.each {|r, c| assert(@t.rows(r).count == c, "Failed on #{r}")}
   end
+
+  def test_delete_row
+    @t.delete_row('fivebug')
+    @t.delete_row('fivebug')
+    assert_raise(VeritableError) { @t.row('fivebug') }
+  end
+
 end
 
 class VeritableTestConnection < Test::Unit::TestCase
