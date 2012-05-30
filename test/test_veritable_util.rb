cd29e145899b10c3077c4621b440785b1d198879
Max Gasner <max@priorknowledge.com>
2012-05-30 11:19:24 -0700

    require veritable

--- test/test_veritable_util.rb
+++ test/test_veritable_util.rb
@@ -1,3 +1,4 @@
+require 'veritable'
 require 'test/unit'
 
 class VeritableTestUtils < Test::Unit::TestCase
@@ -6,6 +7,9 @@ class VeritableTestUtils < Test::Unit::TestCase
      [{'foo' => [1,2,3]}, "foo[]=1&foo[]=2&foo[]=3"],
      [{'foo' => {'a' => 1, 'b' => 2}}, "foo[a]=1&foo[b]=2"],
      [{'foo' => {'a' => 1, 'b' => [1,2,3]}}, "foo[a]=1&foo[b][]=1&foo[b][]=2&foo[b][]=3"]].each {|x|
+     	puts x[0]
+     	puts x[1]
+     	puts Veritable::Util.query_params(x[0])
       assert Veritable::Util.query_params(x[0]) == x[1]
     }
   end
