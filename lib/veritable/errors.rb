dc5706fd6acc35cfd62abc2041cf80f0a54fac06
Max Gasner <max@priorknowledge.com>
2012-05-29 17:35:51 -0700

    change order of block

--- lib/veritable/errors.rb
+++ lib/veritable/errors.rb
@@ -4,13 +4,13 @@ class VeritableError < StandardError
     @message = message
     if opts.is_a? Hash
       @opts = opts
-      @opts.keys.each {|k|
-        class << self
+      class << self
+        @opts.keys.each {|k|
           self.send(:define_method, k.to_sym) {
             @opts[k]
           }
-        end
-      }
+        }
+      end
     end
   end
   def to_s; message; end
