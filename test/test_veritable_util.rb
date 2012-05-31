630a170ed754ad3d07ece8e9f3171e10c448e9f9
Vimal Bhalodia <vimal@priorknowledge.net>
2012-05-31 14:53:02 -0700

    Draft of read and write CSV functions

--- test/test_veritable_util.rb
+++ test/test_veritable_util.rb
@@ -1,10 +1,37 @@
 require 'veritable'
 require 'test/unit'
+require 'tempfile'
 
 class VeritableTestUtils < Test::Unit::TestCase
+
+  def test_write_read_csv():
+    file = Tempfile.new('vtest')
+    file.close
+    begin
+      refrows = [{'_id' => '7', 'ColInt' => 3, 'ColFloat' => 3.1, 'ColCat' => 'a'},
+                 {'_id' => '8', 'ColInt' => 4, 'ColCat' => 'b', 'ColBool' => false},
+                 {'_id' => '9'}]
+      Veritable::Util.write_csv(refrows, file.path)
+      testrows = Veritable::Util.read_csv(file.path)
+      cschema = {
+          'ColInt' => {'type' => 'count'},
+          'ColFloat' => {'type' => 'real'},
+          'ColCat' => {'type' => 'categorical'},
+          'ColBool' => {'type' => 'boolean'}
+          }
+      Veritable::Util.clean_data(testrows, cschema)
+      assert testrows.length == refrows.length
+      (0...testrows.length).each do |i|
+          assert testrows[i] == refrows[i]
+      end
+    ensure
+      file.unlink
+    end
+  end
+
   def test_query_params
-  	# ugh, this is less determinate and needs to be rewritten
-  	# comment out for now, manual inspection confirms the functionality is correct
+    # ugh, this is less determinate and needs to be rewritten
+    # comment out for now, manual inspection confirms the functionality is correct
     # [[{'foo' => 'bar', 'baz' => 2}, "foo=bar&baz=2"],
     #  [{'foo' => [1,2,3]}, "foo[]=1&foo[]=2&foo[]=3"],
     #  [{'foo' => {'a' => 1, 'b' => 2}}, "foo[a]=1&foo[b]=2"],
