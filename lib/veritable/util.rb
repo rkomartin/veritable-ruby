c89b8dbcf0e8572d89c71bd2ffe7d13224b834e9
Max Gasner <max@priorknowledge.com>
2012-05-31 16:09:11 -0700

    draft validate

--- lib/veritable/util.rb
+++ lib/veritable/util.rb
@@ -132,9 +132,204 @@ module Veritable
       end
 
       def validate(rows, schema, opts)
-        validate_schema schema  
-      end
+        schema = Veritable::Schema.new(schema) unless schema.is_a? Veritable::Schema
 
+        # ensure the schema is well-formed
+        schema.validate  
+
+        # store the row numbers of each unique id so that we can warn the user
+        unique_ids = Hash.new
+
+        # store the density of fields
+        field_fill = Hash.new
+        schema.keys.each {|c|
+          field_fill[c] = 0 if c != '_id'
+        }
+
+        # store the number of categories in each categorical column
+        category_counts = Hash.new
+
+        # values which will be converted to true and false in boolean cols if convert_types
+        true_strings = ['true', 't', 'yes', 'y']
+        false_strings = ['false', 'f', 'no', 'n']
+
+        max_cats = 256
+        # be careful before changing the order of any of this logic -- the point is to do this all only once
+        (0...rows.size).each {|i|
+          if opts['assign_ids']
+            rows[i]['_id'] = i.to_s  # number the rows sequentially
+          elsif opts['has_ids']
+            raise VeritableError("Validate -- row #{i} is missing key '_id'", {'row' => i, 'col' => '_id'}) unless r.include? '_id'
+            
+            if opts['convert_type'] # attempt to convert _id to string
+              begin
+                rows[i]['_id'] = rows[i]['_id'].to_s if not rows[i]['_id'].is_a? String
+              rescue
+                raise VeritableError("Validate -- row #{i}, key '_id' cannot be converted to string.". {'row' => i, 'col' => '_id'})
+              end
+            end
+
+            if not rows[i]['_id'].is_a? String # invalid type for _id
+              begin
+                rows[i]['_id'].to_s
+              rescue
+                raise VeritableError("Validate -- row #{i}, key '_id' is not a string.". {'row' => i, 'col' => '_id'})
+              else
+                raise VeritableError("Validate -- row #{i}, key '_id', value #{rows[i]['_id']} is not a string.". {'row' => i, 'col' => '_id'})
+              end
+            end
+            
+            begin
+              check_id rows[i]['_id'] # make sure _id is alphanumeric
+            rescue
+              raise VeritableError("Validate -- row #{i}, key '_id', value #{rows[i]['_id']} contains disallowed characters. Ids must contain only alphanumerics, with underscores and hyphens allowed after the beginning of the id.", {'row' => i, 'col' => '_id'})
+            end
+            
+            if unique_ids.include? rows[i]['_id']
+              raise VeritableError("Validate -- row #{i}, key '_id', value #{rows[i]['_id']} is non-unique, conflicts with row #{unique_ids[rows[i]['_id']]}", {'row' => i, 'col' => '_id'})
+            end
+            
+            unique_ids[rows[i]['_id']] = i
+          elsif rows[i].include? '_id' # no ids, no autoid, but _id column
+            if opts['remove_extra_fields'] # just remove it
+              rows[i].delete '_id'
+            else
+              raise VeritableError("Validate -- row #{i}, key '_id' should not be included.", {'row' => i, 'col' => '_id'})
+            end
+          end
+          rows[i].keys.each {|c|
+            if c != '_id'
+              if not schema.include? c # keys missing from schema
+                if opts['remove_extra_fields'] # remove it
+                  rows[i].delete c
+                else
+                  if not opts['allow_extra_fields'] # or silently allow
+                    raise VeritableError("Row #{i}, key #{c} is not defined in schema", {'row' => i, 'col' => c})
+                  end
+                end
+              elsif rows[i][c].nil? # nil values
+                if opts['remove_nils'] # remove
+                  rows[i].delete c
+                else
+                  if not opts['allow_nones'] # or silently allow
+                    raise VeritableError("Row #{i}, key #{c} should be removed because it is nil", {'row' => i, 'col' => c})
+                  end
+                end
+              else # keys present in schema
+                coltype = schema.type c # check the column type
+                if coltype == 'count'
+                  if opts['convert_types'] # try converting to int
+                    begin
+                      rows[i][c] = rows[i][c].to_i unless rows[i][c].is_a? Fixnum
+                    rescue
+                      rows[i][c] = nil if opts['remove_invalids'] # flag for removal
+                    end
+                  end
+                  if rows[i][c].nil?
+                    rows[i].delete c  # remove flagged values
+                  elsif opts['remove_invalids'] and (rows[i][c].is_a? Fixnum) and (rows[i][c] < 0)
+                    rows[i].delete c
+                  else
+                    if not (rows[i][c].is_a? Fixnum) or not (rows[i][c] >= 0) # catch invalids
+                      raise VeritableError("Validate -- row #{i}, key #{c}, value #{rows[i][c]} is #{rows[i][c].class}, not a non-negative integer.", {'row' => i, 'col' => c})
+                    end
+                  end
+                elsif coltype == 'real'
+                  if opts['convert_types'] # try converting to float
+                    begin
+                      rows[i][c] = rows[i][c].to_f unless rows[i][c].is_a? Float
+                    rescue
+                      rows[i][c] = nil if opts['remove_invalids'] # flag for removal
+                    end
+                  end
+                  if rows[i][c].nil?
+                    rows[i].delete c
+                  else
+                    if not rows[i][c].is_a? Float
+                      raise VeritableError("Validate -- row #{i}, key #{c}, value #{rows[i][c]} is a #{rows[i][c].class}, not a float.", {'row' => i, 'col' => c})
+                    end
+                  end
+                elsif coltype == 'boolean'
+                  if opts['convert_types'] # try converting to bool
+                    lc = rows[i][c].strip.downcase
+                    begin
+                      if true_strings.include? lc
+                        rows[i][c] = true
+                      elsif false_strings.include? lc
+                        rows[i][c] = false
+                      elsif rows[i][c] =~ Regexp.new('\A[0-9]+\z') # note that this behavior differs from what a rubyist might expect; "0" maps to false
+                        if rows[i][c].to_i == 0
+                          rows[i][c] = false
+                        else
+                          rows[i][c] = true
+                        end
+                      end 
+                    rescue
+                      rows[i][c] = nil if opts['remove_invalids'] # flag for removal
+                    end
+                  end
+                  if rows[i][c].nil? # remove flagged values
+                    rows[i].delete c
+                  else
+                    if not [true, false].include? rows[i][c]
+                      raise VeritableError("Validate -- row #{i}, key #{c}, value #{rows[i][c]} is #{rows[i][c].class}, not a boolean", {'rows' => i, 'col' => c})
+                    end
+                  end
+                elsif coltype == 'categorical'
+                  if opts['convert_types'] # try converting to string
+                    begin
+                      rows[i][c] = rows[i][c].to_s unless rows[i][c].is_a? String
+                    rescue
+                      rows[i][c] = nil if opts['remove_invalids'] # flag for removal
+                    end
+                  end
+                  if rows[i][c].nil? # remove flagged values
+                    rows[i].delete c
+                  else
+                    if not rows[i][c].is_a? String # catch invalids
+                      raise VeritableError("Validate -- row #{i}, key #{c}, value #{rows[i][c]} is a #{rows[i][c].class}, not a string", {'rows' => i, 'col' => c})
+                    end
+                    category_counts[c] = Hash.new if not category_counts.include? c # increment count
+                    category_counts[c][rows[i][c]] = 0 if not category_counts[c].include? rows[i][c]
+                    category_counts[c][rows[i][c]] += 1
+                  end
+                else
+                  raise VeritableError("Validate -- didn't recognize column type #{coltype}")
+                end
+              end
+              if not field_fill.include? c and not opts['remove_extra_fields']
+                field_fill[c] = 0
+              end
+              if rows[i].include? c and not rows[i][c].nil?
+                field_fill[c] += 1
+              end
+            end
+          }
+        }
+        category_counts.keys.each {|c|
+          cats = category_counts[c].keys
+          if cats.size > max_cats # too many categories
+            if opts['reduce_categories'] # keep the largest max_cats - 1
+              cats = category_counts[c].sort.keys
+              cats.reverse!
+              category_map = Hash.new
+              (0...cats.size).each {|j|
+                j < max_cats - 1 ? category_map[cats[j]] = cats[j] : category_map[cats[j]] = "Other"
+              }
+              (0...rows.size).each {|i|
+                rows[i][c] = category_map[rows[i][c]] if rows[i].include? c and not rows[i][c].nil?
+              }
+            else
+              raise VeritableError("Validate -- categorical column #{c} has #{category_counts[c].keys.size} unique values which exceeds the limits of #{max_cats}.", {'col' => c})
+            end
+          end
+        }
+        if not opts['allow_empty_columns']
+          field_fill.each {|c, fill|
+            raise VeritableError("Validate -- column #{c} does not have any values", {'col' => c}) if fill == 0
+          }
+        end
+      end
     end
   end
 end
