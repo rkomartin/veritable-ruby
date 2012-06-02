4a8dc24075c0b2743d74989e69d19ec55e319fa7
Ken Robertson <ken@invalidlogic.com>
2012-06-01 19:59:57 -0700

    Syntactic sugar for require_opts and default_opts

--- lib/veritable/object.rb
+++ lib/veritable/object.rb
@@ -9,11 +9,11 @@ module Veritable
 
     private
 
-    def require_opts(keys)
+    def require_opts(*keys)
       keys.each {|k| raise VeritableError.new("Error initializing object -- must provide #{k}") unless @opts.has_key?(k)}
     end
 
-    def default_opts(hash)
+    def default_opts(hash={})
       hash.each {|k, v| @opts[k] = v unless @opts.has_key?(k)}
     end
   end
