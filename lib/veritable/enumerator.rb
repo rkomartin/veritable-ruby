9ae8a448088f1ff7b02b16d343342579cf06af20
Max Gasner <max@priorknowledge.com>
2012-07-16 20:43:40 -0700

    reorder

--- lib/veritable/enumerator.rb
+++ lib/veritable/enumerator.rb
@@ -3,6 +3,66 @@
 # https://github.com/marcandre/backports/blob/master/lib/backports/tools.rb
 
 module Veritable
+  # Metaprogramming utility to make block optional.
+  # Tests first if block is already optional when given options
+  def self.make_block_optional(mod, *methods)
+    options = methods.last.is_a?(Hash) ? methods.pop : {}
+    methods.each do |selector|
+      unless mod.method_defined? selector
+        warn "#{mod}##{selector} is not defined, so block can't be made optional"
+        next
+      end
+      unless options[:force]
+        # Check if needed
+        test_on = options.fetch(:test_on)
+        result =  begin
+                    test_on.send(selector, *options.fetch(:arg, []))
+                  rescue LocalJumpError
+                    false
+                  end
+        next if result.class.name =~ /Enumerator$/
+      end
+      arity = mod.instance_method(selector).arity
+      last_arg = []
+      if arity < 0
+        last_arg = ["*rest"]
+        arity = -1-arity
+      end
+      arg_sequence = ((0...arity).map{|i| "arg_#{i}"} + last_arg + ["&block"]).join(", ")
+
+      alias_method_chain(mod, selector, :optional_block) do |aliased_target, punctuation|
+        mod.module_eval <<-end_eval
+          def #{aliased_target}_with_optional_block#{punctuation}(#{arg_sequence})
+            return to_enum(:#{aliased_target}_without_optional_block#{punctuation}, #{arg_sequence}) unless block_given?
+            #{aliased_target}_without_optional_block#{punctuation}(#{arg_sequence})
+          end
+        end_eval
+      end
+    end
+  end
+  def self.alias_method_chain(mod, target, feature)
+    mod.class_eval do
+      # Strip out punctuation on predicates or bang methods since
+      # e.g. target?_without_feature is not a valid method name.
+      aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
+      yield(aliased_target, punctuation) if block_given?
+
+      with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
+
+      alias_method without_method, target
+      alias_method target, with_method
+
+      case
+        when public_method_defined?(without_method)
+          public target
+        when protected_method_defined?(without_method)
+          protected target
+        when private_method_defined?(without_method)
+          private target
+      end
+    end
+  end
+
   require 'enumerator'
   if (::Enumerable::Enumerator rescue false)
     module Enumerable
@@ -62,66 +122,6 @@ module Veritable
         initialize_without_optional_block(Yielder.new(&block))
       end
       Veritable.alias_method_chain self, :initialize, :optional_block
-    end
-  end
-
-  # Metaprogramming utility to make block optional.
-  # Tests first if block is already optional when given options
-  def self.make_block_optional(mod, *methods)
-    options = methods.last.is_a?(Hash) ? methods.pop : {}
-    methods.each do |selector|
-      unless mod.method_defined? selector
-        warn "#{mod}##{selector} is not defined, so block can't be made optional"
-        next
-      end
-      unless options[:force]
-        # Check if needed
-        test_on = options.fetch(:test_on)
-        result =  begin
-                    test_on.send(selector, *options.fetch(:arg, []))
-                  rescue LocalJumpError
-                    false
-                  end
-        next if result.class.name =~ /Enumerator$/
-      end
-      arity = mod.instance_method(selector).arity
-      last_arg = []
-      if arity < 0
-        last_arg = ["*rest"]
-        arity = -1-arity
-      end
-      arg_sequence = ((0...arity).map{|i| "arg_#{i}"} + last_arg + ["&block"]).join(", ")
-
-      alias_method_chain(mod, selector, :optional_block) do |aliased_target, punctuation|
-        mod.module_eval <<-end_eval
-          def #{aliased_target}_with_optional_block#{punctuation}(#{arg_sequence})
-            return to_enum(:#{aliased_target}_without_optional_block#{punctuation}, #{arg_sequence}) unless block_given?
-            #{aliased_target}_without_optional_block#{punctuation}(#{arg_sequence})
-          end
-        end_eval
-      end
-    end
-  end
-  def self.alias_method_chain(mod, target, feature)
-    mod.class_eval do
-      # Strip out punctuation on predicates or bang methods since
-      # e.g. target?_without_feature is not a valid method name.
-      aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
-      yield(aliased_target, punctuation) if block_given?
-
-      with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
-
-      alias_method without_method, target
-      alias_method target, with_method
-
-      case
-        when public_method_defined?(without_method)
-          public target
-        when protected_method_defined?(without_method)
-          protected target
-        when private_method_defined?(without_method)
-          private target
-      end
     end
   end
 end
