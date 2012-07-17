ac0e13a9f6ed82f19c6c3402c831ab6176e6a580
Max Gasner <max@priorknowledge.com>
2012-07-16 20:41:41 -0700

    syntax

--- lib/veritable/enumerator.rb
+++ lib/veritable/enumerator.rb
@@ -103,24 +103,25 @@ module Veritable
     end
   end
   def self.alias_method_chain(mod, target, feature)
-  mod.class_eval do
-    # Strip out punctuation on predicates or bang methods since
-    # e.g. target?_without_feature is not a valid method name.
-    aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
-    yield(aliased_target, punctuation) if block_given?
+    mod.class_eval do
+      # Strip out punctuation on predicates or bang methods since
+      # e.g. target?_without_feature is not a valid method name.
+      aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
+      yield(aliased_target, punctuation) if block_given?
 
-    with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
+      with_method, without_method = "#{aliased_target}_with_#{feature}#{punctuation}", "#{aliased_target}_without_#{feature}#{punctuation}"
 
-    alias_method without_method, target
-    alias_method target, with_method
+      alias_method without_method, target
+      alias_method target, with_method
 
-    case
-      when public_method_defined?(without_method)
-        public target
-      when protected_method_defined?(without_method)
-        protected target
-      when private_method_defined?(without_method)
-        private target
+      case
+        when public_method_defined?(without_method)
+          public target
+        when protected_method_defined?(without_method)
+          protected target
+        when private_method_defined?(without_method)
+          private target
+      end
     end
   end
 end
